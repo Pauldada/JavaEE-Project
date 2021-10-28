3f6336e53a9306cb235ce9a350b58151d3888c1b
paul_dada <1811426355@qq.com>
2021-10-28 15:41:26 +0800

    绑定Java对象到Thymeleaf表单 & 创建Project实体的CrudRepository

---
+++ pma/src/main/java/com/pauldada/pma/controller/ProjectController.java
@@ -0,0 +1,33 @@
+package com.pauldada.pma.controller;
+
+import com.pauldada.pma.dao.ProjectRepository;
+import com.pauldada.pma.entities.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("projects")
+public class ProjectController {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @GetMapping("/new")
+    public String displayProjectForm(Model model){
+        Project aProject=new Project();
+        model.addAttribute("project",aProject);
+        return "new-project";
+    }
+
+    @PostMapping("/save")
+    public String createProject(Model model,Project project){
+        projectRepository.save(project);
+        return "redirect:/projects/new";
+        //处理保存到数据库的行为
+    }
+
+}
