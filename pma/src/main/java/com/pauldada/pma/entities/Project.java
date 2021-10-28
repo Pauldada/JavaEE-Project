7fcf680b9a853ee63aba0164d0239c33d8260957
paul_dada <1811426355@qq.com>
2021-10-28 15:14:52 +0800

    Project 数据实体

---
+++ pma/src/main/java/com/pauldada/pma/entities/Project.java
@@ -0,0 +1,69 @@
+package com.pauldada.pma.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long projectId;
+    private String name;
+    private String stage;//未开始，已完成，进行中
+    private String description;
+
+    public Project(long projectId, String name, String stage, String description) {
+        this.projectId = projectId;
+        this.name = name;
+        this.stage = stage;
+        this.description = description;
+    }
+
+    public long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(long projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStage() {
+        return stage;
+    }
+
+    public void setStage(String stage) {
+        this.stage = stage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "projectId=" + projectId +
+                ", name='" + name + '\'' +
+                ", stage='" + stage + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    public Project(){
+
+    }
+}
