3c66ea73f243dfd4c6db2e41615a362c38fc295c
paul_dada <1811426355@qq.com>
2021-10-28 17:36:03 +0800

    HomeController修复

--- pma/src/main/java/com/pauldada/pma/controller/HomeController.java
+++ pma/src/main/java/com/pauldada/pma/controller/HomeController.java
@@ -28,15 +28,6 @@ public class HomeController {
 
         List<Student> students = studentRepository.findAll();
         model.addAttribute("students",students);
-        return "main/" +
-                "" +
-                "" +
-                "" +
-                "" +
-                "" +
-                "" +
-                "" +
-                "" +
-                "home";
+        return "main/home";
     }
 }
