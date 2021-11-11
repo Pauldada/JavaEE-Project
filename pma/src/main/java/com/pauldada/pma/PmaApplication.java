8822b7431ab3c3be5b2a0aeaaf3768d9dbe1b817
Paul_dada <1811426355@qq.com>
2021-11-11 16:45:04 +0800

    链接和配置postgresql数据库

--- pma/src/main/java/com/pauldada/pma/PmaApplication.java
+++ pma/src/main/java/com/pauldada/pma/PmaApplication.java
@@ -1,16 +1,8 @@
 package com.pauldada.pma;
 
-import com.pauldada.pma.dao.ProjectRepository;
-import com.pauldada.pma.dao.StudentRepository;
-import com.pauldada.pma.entities.Project;
-import com.pauldada.pma.entities.Student;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class PmaApplication {
