4486d3d608ab8376c9d3000f7ec10f41d5389e45
Paul_dada <1811426355@qq.com>
2021-11-18 17:05:58 +0800

    Spring Security 3:基于JDBC的安全机制

--- pma/src/main/java/com/pauldada/pma/security/SecurityConfiguration.java
+++ pma/src/main/java/com/pauldada/pma/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.pauldada.pma.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,23 +10,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-        .withUser("myuser")
-        .password("123")
-        .roles("USER")
-        .and()
-        .withUser("paul_dada")
-        .password("010329")
-        .roles("ADMIN")
-        .and()
-        .withUser("managerUser")
-        .password("123")
-        .roles("USER");
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username,password,enabled "+"FROM users WHERE username = ?")
+                .authoritiesByUsernameQuery("SELECT username,authority "+"FROM authorities WHERE username = ?");
     }
 
     @Bean
@@ -37,6 +35,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/projects/new").hasRole("ADMIN")
+                .antMatchers("/students/new").hasRole("ADMIN")
+                .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/").authenticated().and().formLogin();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 }
