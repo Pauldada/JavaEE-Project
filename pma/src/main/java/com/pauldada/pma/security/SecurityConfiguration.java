ec3c6c0b0bf2a55693ed475ac24e035c7352f68f
Paul_dada <1811426355@qq.com>
2021-11-18 16:57:25 +0800

    Spring Security 2:基于内容的授权机制 {In-Memory Authorisation}

--- pma/src/main/java/com/pauldada/pma/security/SecurityConfiguration.java
+++ pma/src/main/java/com/pauldada/pma/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.pauldada.pma.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -15,16 +16,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
         .withUser("myuser")
-        .password("pass")
+        .password("123")
         .roles("USER")
         .and()
         .withUser("paul_dada")
         .password("010329")
+        .roles("ADMIN")
+        .and()
+        .withUser("managerUser")
+        .password("123")
         .roles("USER");
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/projects/new").hasRole("ADMIN")
+                .antMatchers("/").authenticated().and().formLogin();
     }
 }
